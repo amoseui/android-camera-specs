6dad690484b2e500e00a986d69ddf73f55f276fe
Amos Lim <amoseui@gmail.com>
2024-01-15 19:35:24 +0900

    Add simple compose view of Grid Cards for camera ids

---
+++ app/src/main/java/com/amoseui/camerainfo/CameraIdScreen.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2024 amoseui (Amos Lim)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.amoseui.camerainfo
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CameraIds(
+    cameraResources: List<CameraResource>,
+    innerPadding: PaddingValues,
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.padding(innerPadding),
+    ) {
+        items(cameraResources) { cameraResource ->
+            CameraIdCard(
+                cameraResource = cameraResource,
+            )
+        }
+    }
+}
+
+@Composable
+fun CameraIdCard(
+    cameraResource: CameraResource,
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp),
+    ) {
+        Text(
+            text = "Camera ID: ${cameraResource.id}",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
